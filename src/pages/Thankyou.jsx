c7665e465f51dd30fc8d941dcc9c3c3efd2d2ee4
tina <kaltrinamerovcii@gmail.com>
2024-04-28 19:50:38 +0200

    thankyoupage fixes

--- src/pages/Thankyou.jsx
+++ src/pages/Thankyou.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'; // Import Link from React Router
+
 import Footer from "../components/UI/Footer";
 
 const Thankyou = () => {
@@ -5,9 +7,16 @@ const Thankyou = () => {
         <main>
             <div className="flex items-center justify-center w-full h-screen bg-gradient-to-br from-blue-600 to-fuchsia-600">
                 <div className="text-white text-center p-8">
-                    <h2 className="text-3xl font-titles mb-4">Welcome to Eventopia</h2>
-                    <p className="text-lg">Thank you for joining us!</p>
-                    <p className="text-lg">Log in again for more</p>
+                    <h2 className="text-3xl font-titles mb-4">Welcome to Eventopia!</h2>
+                    <p className="text-2xl mb-6">Thank you for joining us!</p>
+                    <p className="text-2xl mb-12">Log in for more</p>
+                    {/* Adjusted the margin-bottom to increase distance between text and button */}
+                    <div className="flex items-start justify-center mb-12">
+                        {/* Use Link component for navigation */}
+                        <Link to="/login">
+                            <button type="login" className="w-full font-titles sm:w-80 bg-black text-white px-4 py-4 rounded-md mr-4 sm:mr-0 transition duration-300 hover:bg-[#e70ce3aa]">Log In</button>
+                        </Link>
+                    </div>
                 </div>
             </div>
             <Footer />
